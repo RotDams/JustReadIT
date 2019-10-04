b8608fd8c1a541f08684bc83de4a14d69d13feac
Dams <dchampeyroux.dc@gmail.com>
2019-10-04 13:10:03 +0200

    Feat (main.c): Adding CutImage and getpixel

--- main.c
+++ main.c
@@ -1,8 +1,66 @@
 #include <SDL2/SDL.h>
 
-int main() {
-	SDL_Surface *image;
-	image = SDL_LoadBMP("src/assets/chat.bmp");
-	printf("Height: %d", image->h);
-	return 0;
+
+Uint32 getpixel(SDL_Surface *surface, int x, int y) {
+    int bpp = surface->format->BytesPerPixel;
+    Uint8 *p = (Uint8 *) surface->pixels + y * surface->pitch + x * bpp;
+    return *p;
 }
+
+SDL_Surface CutImage(SDL_Surface *Source,int startX,int startY,int W,int H){
+    SDL_Rect decoupe, pos;
+    SDL_Surface *ecran;
+
+    //Init of the new image
+    ecran = SDL_CreateRGBSurface(0,W,H,32,
+                                 0xff000000,0x00ff0000,
+                                 0x0000ff00,0x000000ff);
+
+    //init of the start of the édition
+    pos.x = 0;
+    pos.y = 0;
+
+    decoupe.x = startX; // X start
+    decoupe.y = startY; // Y start
+    decoupe.w = W;  // width
+    decoupe.h = H;  // height
+
+    SDL_BlitSurface(Source, &decoupe, ecran, &pos);
+    SDL_SaveBMP(ecran, "ImageTest.bmp");//TODO Debug mode
+
+    return *ecran;
+}
+
+void takeAllPixelColor(SDL_Surface *image) {
+    int r, g, b;
+    for (int y = 0; y < image->h; ++y) {
+        for (int x = 0; x < image->w; ++x) {
+            printf("%i %i \n", x, y);
+            SDL_GetRGB(getpixel(image, x, y), image->format, &r, &g, &b);
+            printf("pixel: r(%i) g(%i) b(%i) \n ", r, g, b);
+        }
+    }
+}
+
+SDL_Surface TakeParagraphe(SDL_Surface *image) {
+
+}
+
+void TakeWords(SDL_Surface *Paragraphe) {
+
+}
+
+void TakeLetters(SDL_Surface *Words) {
+
+
+}
+
+
+int main() {
+    SDL_Surface *image;
+    image = SDL_LoadBMP("src/assets/chat.bmp");
+    //takeAllPixelColor(image);
+    CutImage(image,100,100,400,200);
+    return 0;
+}
+
