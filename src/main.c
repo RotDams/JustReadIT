9057369d2065b48d22ac39d7cabbf9e87ac695ec
quentin.briolant <quentin.briolant@epita.fr>
2019-10-17 19:24:43 +0200

    fix: Remove unuse images

--- src/main.c
+++ src/main.c
@@ -2,40 +2,34 @@
 #include <unistd.h>
 #include "libs/image_segmentation/index.h"
 
-int main(){//int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
-//    if (argc == 1) {
-//        printf("Error: Please specify the path of the image\nEx: ./output.exe -p \"my_image.bmp\"\n");
-//        return EXIT_FAILURE;
-//    }
-//
-//    // List of all possible arguments
-//    char *path = "";
-//    int is_dev_mode = 0;
-//
-//    // for each arg
-//    int i = 1;
-//    while (i < argc) {
-//        if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--dev") == 0) {
-//            is_dev_mode = 1;
-//
-//        } else if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--path") == 0) {
-//            i++;
-//            if (i >= argc) {
-//                printf("Error: Please specify the path of the image\nEx: ./output.exe -p \"my_image.bmp\"\n");
-//                return EXIT_FAILURE;
-//            }
-//            path = argv[i];
-//        }
-//        i++;
-//    }
-//
-//    if (access(path, F_OK) == -1) {
-//        printf("Error: The file does not exist.\n");
-//        return EXIT_FAILURE;
-//    }
+	if (argc == 1) {
+		printf("Error: Please specify the path of the image\nEx: ./output.exe -p \"my_image.bmp\"\n");
+		return EXIT_FAILURE;
+	}
+	// List of all possible arguments
+	char *path = "";
 
-    List paragraphs = image_segmentation("src/assets/image_white_bg.bmp");
-    print_list(paragraphs);
-    return 0;
+	// for each arg
+	int i = 1;
+	while (i < argc) {
+		if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--path") == 0) {
+			i++;
+			if (i >= argc) {
+				printf("Error: Please specify the path of the image\nEx: ./output.exe -p \"my_image.bmp\"\n");
+				return EXIT_FAILURE;
+			}
+			path = argv[i];
+		}
+		i++;
+	}
+	if (access(path, F_OK) == -1) {
+		printf("Error: The file does not exist.\n");
+		return EXIT_FAILURE;
+	}
+
+	List paragraphs = image_segmentation(path);
+	print_list(paragraphs);
+	return 0;
 }
