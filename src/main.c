427ada3dd9a645fc05d1dc7176f7e8d3969e93fa
Quentin Briolant <quentin.briolant@epita.fr>
2019-12-01 22:28:25 +0100

    refractor (src): upade code

--- src/main.c
+++ src/main.c
@@ -1,50 +1,9 @@
-#include <SDL/SDL.h>
-#include <unistd.h>
 #include "libs/image_segmentation/rotation.h"
 #include "libs/interface/interface.h"
-#include "libs/image_segmentation/index.h"
-#include "libs/neural_network/neuralnetwork.h"
-#include "libs/neural_network/index.h"
 
 int main(int argc, char *argv[]) {
 
     // Check if we want the interface
     init_interface(argc, argv);
     return 0;
-
-    // Else, we will call the program with arguments
-
-
-    // List of all possible arguments
-    char *path = "";
-
-    // for each arg
-    int i = 1;
-    while (i < argc) {
-        if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--path") == 0) {
-            i++;
-            if (i >= argc) {
-                printf("Error: Please specify the path of the image\nEx: ./output.exe -p \"my_image.bmp\"\n");
-                return EXIT_FAILURE;
-            }
-            path = argv[i];
-        }
-        i++;
-    }
-
-    if (access(path, F_OK) == -1) {
-        printf("Error: The file does not exist.\n");
-        return EXIT_FAILURE;
-    }
-
-
-    // List paragraphs = image_segmentation(path);
-    // print_list(paragraphs);
-
-
-
-
-
-    return 0;
-
 }
